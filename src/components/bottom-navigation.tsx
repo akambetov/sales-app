234d9c6df32916074a01bb70f85c71ef65f82510
Almat Kambetov <Almat.Kambetov@berekebank.kz>
2026-04-08 22:46:22 +0500

    fix: bottom-navigation position; layout height

--- src/components/bottom-navigation.tsx
+++ src/components/bottom-navigation.tsx
@@ -13,42 +13,31 @@ const navItems = [
   { id: 'results', label: 'Результат', icon: BarChart3, path: PATHS.results }
 ] as const
 
-const BottomNav = ({ path }: { path: string }) => {
+export const BottomNavigation = () => {
+  const { pathname } = useLocation()
   const navigate = useNavigate()
 
   return (
-    <div className="fixed bottom-4 left-1/2 z-30 w-full max-w-107.5 -translate-x-1/2 px-4">
-      <div className="grid grid-cols-6 rounded-[28px] border border-slate-200 bg-white p-2 shadow-2xl">
-        {navItems.map((item) => {
-          const Icon = item.icon
-          const isActive = path === item.path
-          return (
-            <button
-              key={item.id}
-              onClick={() => {
-                navigate(item.path)
-              }}
-              className={cn(
-                'flex flex-col items-center justify-center rounded-2xl px-1 py-2 text-[10px] font-medium cursor-pointer',
-                isActive ? 'bg-slate-900 text-white' : 'text-slate-500'
-              )}
-            >
-              <Icon size={17} />
-              <span className="mt-1 leading-none">{item.label}</span>
-            </button>
-          )
-        })}
-      </div>
-    </div>
-  )
-}
-
-export const BottomNavigation = () => {
-  const { pathname } = useLocation()
-
-  return (
-    <div className="min-h-screen bg-slate-100 py-4">
-      <BottomNav path={pathname} />
+    <div className="grid grid-cols-6 rounded-[28px] border border-slate-200 bg-white p-2 shadow-2xl">
+      {navItems.map((item) => {
+        const Icon = item.icon
+        const isActive = pathname === item.path
+        return (
+          <button
+            key={item.id}
+            onClick={() => {
+              navigate(item.path)
+            }}
+            className={cn(
+              'flex flex-col items-center justify-center rounded-2xl px-1 py-2 text-[10px] font-medium cursor-pointer',
+              isActive ? 'bg-slate-900 text-white' : 'text-slate-500'
+            )}
+          >
+            <Icon size={17} />
+            <span className="mt-1 leading-none">{item.label}</span>
+          </button>
+        )
+      })}
     </div>
   )
 }
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,8 +3,10 @@ import { Outlet } from 'react-router'
 import { BottomNavigation } from './bottom-navigation'
 
 export const Layout = () => (
-  <div className="pb-28">
-    <Outlet />
+  <div className="flex flex-col h-screen overflow-hidden">
+    <div className="flex-1 overflow-auto">
+      <Outlet />
+    </div>
     <BottomNavigation />
   </div>
 )
