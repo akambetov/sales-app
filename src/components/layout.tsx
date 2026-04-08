10ab27f718b42dbbb72e7ed25fecea120859c702
Almat Kambetov <Almat.Kambetov@berekebank.kz>
2026-04-09 00:22:08 +0500

    fix: layout height

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router'
 import { BottomNavigation } from './bottom-navigation'
 
 export const Layout = () => (
-  <div className="flex flex-col h-screen overflow-hidden">
+  <div className="flex flex-col h-dvh overflow-hidden">
     <div className="flex-1 overflow-auto">
       <Outlet />
     </div>
